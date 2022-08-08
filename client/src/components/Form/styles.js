1bef1f5bc258bfb40aafbad00ef9e3d5af7bbccf
Muhammad Chaudhary <97010233+MoC1099@users.noreply.github.com>
2022-08-08 18:49:41 -0400

    Add files via upload

---
+++ client/src/components/Form/styles.js
@@ -0,0 +1,26 @@
+// styles for the Form, you can find multiple version fo styles on githubs.
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
